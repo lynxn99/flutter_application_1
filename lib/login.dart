8566aaa25db2f1f0d1b7feb6a0f292f7f58ac4a4
lynxn99 <linsutilitymail@gmail.com>
2025-05-09 15:00:34 +0530

    icons

--- lib/login.dart
+++ lib/login.dart
@@ -8,6 +8,7 @@ class Login extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
+          Positioned(top: 5, left: 5, child: Icon(Icons.arrow_back_ios)),
           Positioned.fill(
             child: Image.asset("assets/beautiful.jpg", fit: BoxFit.cover),
           ),
@@ -20,11 +21,13 @@ class Login extends StatelessWidget {
                   Text(
                     "WELCOME BACK",
                     style: TextStyle(
-                      color: Colors.white,
+                      color: const Color.fromARGB(255, 0, 0, 0),
                       fontSize: 25,
                       fontWeight: FontWeight.w200,
                     ),
                   ),
+
+                  SizedBox(height: 16),
 
                   TextField(
                     decoration: InputDecoration(
@@ -33,10 +36,10 @@ class Login extends StatelessWidget {
                         icon: Icon(Icons.account_circle),
                       ),
                       filled: true,
-                      fillColor: Colors.grey,
+                      fillColor: const Color.fromARGB(255, 255, 255, 255),
                       hintText: "enter username",
                       border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
+                        borderRadius: BorderRadius.circular(25),
                       ),
                     ),
                   ),
@@ -50,23 +53,98 @@ class Login extends StatelessWidget {
                         icon: Icon(Icons.account_circle),
                       ),
                       filled: true,
-                      fillColor: Colors.grey,
+                      fillColor: const Color.fromARGB(255, 255, 255, 255),
                       hintText: "password",
                       border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
+                        borderRadius: BorderRadius.circular(25),
                       ),
                     ),
                   ),
                   Padding(
                     padding: const EdgeInsets.only(left: 240),
-                    child: TextButton(onPressed:(){},child:Text("forgot password?",style: TextStyle(color: Colors.white))),
-                  )
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "forgot password?",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(
+                    height: 50,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.yellow,
+                        foregroundColor: Colors.black,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      child: Text("login"),
+                    ),
+                  ),
+
+                  SizedBox(height: 16),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          color: Colors.grey,
+                          thickness: 2,
+                          endIndent: 2,
+                          indent: 2,
+                        ),
+                      ),
+                      Text(
+                        "Or login with",
+                        style: TextStyle(color: Colors.white, fontSize: 15),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          color: Colors.grey,
+                          thickness: 2,
+                          endIndent: 2,
+                          indent: 2,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  SizedBox(height: 500),
+                  Row(children: [loginfun("assets/facebook.jpg"), loginfun("assets/Aadhaar.png"), loginfun("google.png")]),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Don't have an account register now",
+                      style: TextStyle(
+                        color: const Color.fromARGB(255, 255, 255, 255),
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
-          
           ),
         ],
+      ),
+    );
+  }
+
+  Widget loginfun(String imagepath) {
+    return Container(
+      height: 50,
+      width: 50,
+      decoration: BoxDecoration(
+        color: Colors.black,
+        border: Border.all(color: Colors.amber),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(12),
+        child: Image.asset(imagepath),
       ),
     );
   }
--- lib/register.dart
+++ lib/register.dart
@@ -11,57 +11,143 @@ class Register extends StatelessWidget {
           Positioned.fill(
             child: Image.asset("assets/artificial.jpg", fit: BoxFit.cover),
           ),
-          Align(alignment: Alignment.topCenter,
-          child:  Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-          Text("Hello Register",style: TextStyle(color:const Color.fromARGB(255, 255, 255, 255) ,fontSize: 23,fontWeight: FontWeight.w200),),
-          Text("to get started",style: TextStyle(color:const Color.fromARGB(255, 255, 255, 255) ,fontSize: 23,fontWeight: FontWeight.w200),),
-          SizedBox(height: 25,),
-          TextField(
-            decoration: InputDecoration(
-               suffixIcon: IconButton(onPressed: (){}, icon:Icon(Icons.account_circle)),
-                filled: true,
-               fillColor: Colors.grey,
-                hintText: "create username",
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)))
-               ),
-               SizedBox(height: 15,),
-          TextField(
-            decoration: InputDecoration(
-               suffixIcon: IconButton(onPressed: (){}, icon:Icon(Icons.account_circle)),
-                filled: true,
-               fillColor: Colors.grey,
-                hintText: "Email address",
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)))
-                 ),
-                 SizedBox(height: 15,),
-          TextField(
-            decoration: InputDecoration(
-               suffixIcon: IconButton(onPressed: (){}, icon:Icon(Icons.visibility)),
-                filled: true,
-               fillColor: Colors.grey,
-                hintText: "enter password",
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)))
-                ),
-                SizedBox(height: 15,),
-          TextField(
-            decoration: InputDecoration(
-               suffixIcon: IconButton(onPressed: (){}, icon:Icon(Icons.visibility)),
-                filled: true,
-               fillColor: Colors.grey,
-                hintText: "confirm password",
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)))
-                 ),
-                 SizedBox(height: 15,),
-                 
-          ],
-          ),
-      ))
-        ]));
-  
+          Align(
+            alignment: Alignment.topCenter,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.arrow_back_ios),
+                    ),
+                  ),
+                  Text(
+                    "Hello Register",
+                    style: TextStyle(
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                      fontSize: 23,
+                      fontWeight: FontWeight.w200,
+                    ),
+                  ),
+                  Text(
+                    "to get started",
+                    style: TextStyle(
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                      fontSize: 23,
+                      fontWeight: FontWeight.w200,
+                    ),
+                  ),
+                  SizedBox(height: 25),
+                  TextField(
+                    decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.account_circle),
+                      ),
+                      filled: true,
+                      fillColor: Colors.grey,
+                      hintText: "create username",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  TextField(
+                    decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.account_circle),
+                      ),
+                      filled: true,
+                      fillColor: Colors.grey,
+                      hintText: "Email address",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  TextField(
+                    decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.visibility),
+                      ),
+                      filled: true,
+                      fillColor: Colors.grey,
+                      hintText: "enter password",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  TextField(
+                    decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.visibility),
+                      ),
+                      filled: true,
+                      fillColor: Colors.grey,
+                      hintText: "confirm password",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ),
 
+                  SizedBox(height: 15),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          color: Colors.grey,
+                          thickness: 2,
+                          indent: 2,
+                          endIndent: 2,
+                        ),
+                      ),
+                      Text(
+                        "or register with",
+                        style: TextStyle(color: Colors.white, fontSize: 15),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          color: Colors.grey,
+                          thickness: 2,
+                          indent: 2,
+                          endIndent: 2,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  SizedBox(height: 10),
+
+                  SizedBox(
+                    height: 32,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.black,
+                      ),
+                      child: Text("register"),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          
+        ],
+      ),
+    );
   }
-    
-  }
+}
